09f81b2195bcbdf0d48034c77d5b07d671cfaf20
allifizzuddin89 <allifizzuddin@gmail.com>
2023-03-02 02:35:09 +0800

    test via shell

--- trader_joes_inventory/trader_joes_inventory/spiders/main.py
+++ trader_joes_inventory/trader_joes_inventory/spiders/main.py
@@ -34,10 +34,10 @@ class MainSpider(scrapy.Spider):
         "AMCV_B5B4708F5F4CE8D80A495ED9%40AdobeOrg": "-2121179033%7CMCIDTS%7C19418%7CMCMID%7C68318522979365702402480968825318066361%7CMCOPTOUT-1677700078s%7CNONE%7CvVersion%7C5.3.0"
     }
 
-    body = '{"operationName":"SearchProducts","variables":{"storeCode":"TJ","availability":"1","published":"1","categoryId":8,"currentPage":1,"pageSize":15},"query":"query SearchProducts($categoryId: String, $currentPage: Int, $pageSize: Int, $storeCode: String = \"TJ\", $availability: String = \"1\", $published: String = \"1\") {\n  products(\n    filter: {store_code: {eq: $storeCode}, published: {eq: $published}, availability: {match: $availability}, category_id: {eq: $categoryId}}\n    currentPage: $currentPage\n    pageSize: $pageSize\n  ) {\n    items {\n      sku\n      item_title\n      category_hierarchy {\n        id\n        name\n        __typename\n      }\n      primary_image\n      primary_image_meta {\n        url\n        metadata\n        __typename\n      }\n      sales_size\n      sales_uom_description\n      price_range {\n        minimum_price {\n          final_price {\n            currency\n            value\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      retail_price\n      fun_tags\n      item_characteristics\n      __typename\n    }\n    total_count\n    pageInfo: page_info {\n      currentPage: current_page\n      totalPages: total_pages\n      __typename\n    }\n    aggregations {\n      attribute_code\n      label\n      count\n      options {\n        label\n        value\n        count\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"}'
+    body = '{"operationName":"SearchProducts","variables":{"storeCode":"TJ","availability":"1","published":"1","categoryId":8,"currentPage":1,  "pageSize":15},"query":"query SearchProducts($categoryId: String, $currentPage: Int, $pageSize: Int, $storeCode: String = \"TJ\", $availability: String = \"1\", $published: String = \"1\") {\n  products(\n    filter: {store_code: {eq: $storeCode}, published: {eq: $published}, availability: {match: $availability}, category_id: {eq: $categoryId}}\n    currentPage: $currentPage\n    pageSize: $pageSize\n  ) {\n    items {\n      sku\n      item_title\n      category_hierarchy {\n        id\n        name\n        __typename\n      }\n      primary_image\n      primary_image_meta {\n        url\n        metadata\n        __typename\n      }\n      sales_size\n      sales_uom_description\n      price_range {\n        minimum_price {\n          final_price {\n            currency\n            value\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      retail_price\n      fun_tags\n      item_characteristics\n      __typename\n    }\n    total_count\n    pageInfo: page_info {\n      currentPage: current_page\n      totalPages: total_pages\n      __typename\n    }\n    aggregations {\n      attribute_code\n      label\n      count\n      options {\n        label\n        value\n        count\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"}'
 
     def start_requests(self):
-        Request(
+        yield Request(
             url=self.url,
             method='POST',
             dont_filter=True,
@@ -47,4 +47,4 @@ class MainSpider(scrapy.Spider):
         )
     
     def parse(self, response):
-        inspect_response(self,response)
+        inspect_response(response, self)
