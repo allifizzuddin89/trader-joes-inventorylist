f4bff50096bf767e34b0ba1d46bf2a5d740951b3
allifizzuddin89 <allifizzuddin@gmail.com>
2023-03-02 02:35:02 +0800

    test via shell

--- trader_joes_inventory/trader_joes_inventory/settings.py
+++ trader_joes_inventory/trader_joes_inventory/settings.py
@@ -92,6 +92,8 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
+########################################################################
+
 import copy
 from colorlog import ColoredFormatter
 import scrapy.utils.log
